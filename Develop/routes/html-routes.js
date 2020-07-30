da9e4e45257536279862fc3601a47f010f83ba8e
enrightcw <58670012+enrightcw@users.noreply.github.com>
2020-07-30 18:32:25 -0400

    cobbled together some API routes to see if I can get the db working with the existing code

--- Develop/routes/html-routes.js
+++ Develop/routes/html-routes.js
@@ -11,33 +11,3 @@ module.exports = function(app) {
   });
 };
 
-
-// app.get("/", (req, res) => {
-//     res.send(path.join(__dirname + "/public/index.html"));
-//   });
-  
-//   app.get("/exercise", (req, res) => {
-//     res.send(path.join(__dirname + "/public/exercise.html"));
-//   });
-  
-
-//   // app.get("/workout", (req, res) => {
-//   //   res.send(path.join(__dirname + "./public/workout.html"));
-//   // });
-  
-//   // Route to post our form submission to mongoDB via mongoose
-//   app.post("/submit", ({body}, res) => {
-//       const workout = new Workout(body);
-  
-//     Workout.create(workout)
-//       .then(dbTracker => {
-//         // If saved successfully, send the the new User document to the client
-//         res.json(dbTracker);
-//       })
-//       .catch(err => {
-//         // If an error occurs, send the error to the client
-//         res.json(err);
-//       });
-//   });
-
-
